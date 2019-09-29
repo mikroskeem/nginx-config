7c7d7299b5dc4f8ecc60a4c1f35e9a53a47ba737
Markkus Millend <mllnd@users.noreply.github.com>
2019-09-29 17:56:28 +0300

    Add a helper script for generating self-signed certs

---
+++ scripts/gen_ss.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# C  -> Country code
+# ST -> State
+# L  -> City
+# O  -> Organization
+# CN -> Common Name
+C="xx"
+ST="undefined"
+L="undefined"
+O="undefined"
+CN="undefined"
+
+duration=3650
+
+# Eval args
+for x in "${@}"; do
+    echo ">>> Setting '${x}'"
+    eval "${x}"
+done
+
+subject="/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN}"
+echo ">>> Certificate subject: ${subject}"
+echo ">>> Certificate duration: ${duration}d"
+
+openssl req -x509 -nodes -newkey rsa:4096 \
+    -days "${duration}" \
+    -subj "${subject}" \
+    -keyout selfsigned.key.pem -out selfsigned.cert.pem
